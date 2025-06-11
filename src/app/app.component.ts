90468b531d1cc385e16bd917d5921d2c34b95508
Luís Daniel <danielmacmin@gmail.com>
2025-06-11 18:15:37 -0300

    contatos + exp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PlanningComponent } from './pages/planning/planning.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContatosComponent } from './pages/contatos/contatos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, PlanningComponent, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, PlanningComponent, NavbarComponent, ContatosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
